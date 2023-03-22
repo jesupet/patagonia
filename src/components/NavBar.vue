4c17e2a091d85bde26e2179e3077774aba2b1ec0
MariaJesús <jesup93@gmail.com>
2023-03-22 13:43:59 -0300

    INSTALADO BOOTSTRAP

---
+++ src/components/NavBar.vue
@@ -0,0 +1,68 @@
+<template>
+  <nav class="navbar navbar-expand-lg">
+    <div class="container-fluid mt-4">
+      <a class="navbar-brand" href="#"><img class="logo-navbar" src="" alt="logo vanilla"></a>
+      <button class="navbar-toggler navbar-dark" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link class="nav-link align-middle" aria-current="page" to="/">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/">Nosotros</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/">¿Qué Hacemos?</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/">Proyectos</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/">Han Confiado</router-link>
+          </li>
+          <li class="nav-item">
+            <PurpleButton :to="'/'" :buttonText="'Hablemos!'"/>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style>
+  nav {
+    background-color: #1b1629;
+    color: #fff;
+    height: 75px;
+  }
+  .navbar-toggler {
+    border: 0px;
+    color: #fff;
+  }
+  .logo-navbar {
+    height: 70px;
+    width: auto;
+    margin-top: 30px;
+    margin-left: 40px;
+  }
+  .nav-item {
+    padding: 20px;
+    text-decoration: none;
+    display: flex;
+    align-items: center;
+  }
+  .nav-link:hover {
+    color: #6d44bc;
+  }
+  .nav-link {
+    color: #fff;
+    text-transform: uppercase;
+    font-size: 14px;
+    padding: 0px;
+  }
+  .rounded-pill {
+    text-align: center;
+  }
+</style>
---
+++ src/views/HomePatagonia.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <header>
+
+    </header>
+    <section id="aysen">
+
+    </section>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'HomePatagonia',
+  components: {
+  
+  }
+}
+</script>

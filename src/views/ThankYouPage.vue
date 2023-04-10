9cd656a60e262362952cfc5a49090ff228f7c3f5
MariaJesús <jesup93@gmail.com>
2023-04-10 15:46:22 -0400

    PAGINA GRACIAS LISTA

--- src/views/ThankYouPage.vue
+++ src/views/ThankYouPage.vue
@@ -1,14 +1,14 @@
 <template>
-  <div>
+  <div class="mi-pagina-vue">
     <div class="background-image7 big-cont">
       <div class="container-fluid text-center thanks-cont">
         <img src="../assets/patagonia_logo.png" alt="logo futuro patagonia">
-        <h1 class="titulo">¡Gracias!</h1>
-        <p class="bajada">Te mantendremos al tanto de todos nuestros proyectos.</p>
-        <GreenButton :to="{name: 'home', hash: '#'+''}" :buttonText="'Volver'"/>
+        <h1 class="titulo big-txt espaciado">¡Gracias!</h1>
+        <p class="bajada espaciado">Te mantendremos al tanto de todos nuestros proyectos.</p>
+        <GreenButton :to="{name: 'home', hash: '#'+''}" :buttonText="'Volver'" class="espaciado"/>
       </div>
     </div>
-    <div class="footer-text text-center align-items-center">
+    <div class="footer-text text-center align-items-center foot-tks">
       <p>Síguenos en <a target="_blank" href="https://www.instagram.com/futuropatagonia/">instagram</a> para estar al tanto de todas nuestras novedades y nuevos lanzamientos.</p>
     </div>
   </div>
@@ -30,10 +30,6 @@ export default {
 </script>
 
 <style scoped>
-body, html {
-  height: 100vh;
-  overflow-y: hidden;
-}
-
+  
 
 </style>
